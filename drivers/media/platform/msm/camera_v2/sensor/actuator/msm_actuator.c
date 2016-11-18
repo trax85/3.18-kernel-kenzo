03670fa927d1147ed7a4f7d99d7103692c1290cb
TheStrix <parthbhatia98@gmail.com>
2016-11-18 13:14:13 +0530

    drivers: camera_v2: Import Xiaomi HM Note3 code
    * ifdef kenzo code
    * Squash of the following commits:
      https://github.com/CyanogenMod/android_kernel_xiaomi_msm8956/commit/88214c43659c12afac12ff2062741f05d55f8153
      https://github.com/CyanogenMod/android_kernel_xiaomi_msm8956/commit/2d861097ffef66de3bfd03b6276cd515a1ed680d
      https://github.com/CyanogenMod/android_kernel_xiaomi_msm8956/commit/e82a4d84e7998f3acd3afcb0eb627f81af78c668
      https://github.com/CyanogenMod/android_kernel_xiaomi_msm8956/commit/2ce8d89dd7cf045591ea5b3eb1b22cb51ee0b302
    
    Change-Id: I6080b83cd772aba233ee921a1e4cf232e6f86cd4
    Signed-off-by: TheStrix <parthbhatia98@gmail.com>

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -106,6 +106,13 @@ static void msm_actuator_parse_i2c_params(struct msm_actuator_ctrl_t *a_ctrl,
 	write_arr = a_ctrl->reg_tbl;
 	i2c_tbl = a_ctrl->i2c_reg_tbl;
 
+#ifdef CONFIG_MACH_XIAOMI_KENZO
+	if(i2c_tbl == NULL) {
+		pr_err("i2c_tbl is null!\n");
+		return;
+	}
+#endif
+
 	for (i = 0; i < size; i++) {
 		if (write_arr[i].reg_write_type == MSM_ACTUATOR_WRITE_DAC) {
 			value = (next_lens_position <<
@@ -140,6 +147,37 @@ static void msm_actuator_parse_i2c_params(struct msm_actuator_ctrl_t *a_ctrl,
 				i2c_byte1 = (value & 0xFF00) >> 8;
 				i2c_byte2 = value & 0xFF;
 			}
+#ifdef CONFIG_MACH_XIAOMI_KENZO
+		} else if (write_arr[i].reg_write_type == MSM_ACTUATOR_WRITE_DAC_DW9718S ||
+			write_arr[i].reg_write_type == MSM_ACTUATOR_WRITE_DAC_DW9761B) {
+			value = (next_lens_position <<
+				write_arr[i].data_shift) |
+				((hw_dword & write_arr[i].hw_mask) >>
+				write_arr[i].hw_shift);
+
+			if (write_arr[i].reg_addr != 0xFFFF) {
+				i2c_byte1 = write_arr[i].reg_addr;
+				i2c_byte2 = value;
+				if (size != (i+1)) {
+					i2c_byte2 = (value & 0xFF00) >> 8;
+					CDBG("byte1:0x%x, byte2:0x%x\n",
+						i2c_byte1, i2c_byte2);
+					i2c_tbl[a_ctrl->i2c_tbl_index].
+						reg_addr = i2c_byte1;
+					i2c_tbl[a_ctrl->i2c_tbl_index].
+						reg_data = i2c_byte2;
+					i2c_tbl[a_ctrl->i2c_tbl_index].
+						delay = 0;
+					a_ctrl->i2c_tbl_index++;
+					i++;
+					i2c_byte1 = write_arr[i].reg_addr;
+					i2c_byte2 = value & 0xFF;
+				}
+			} else {
+				i2c_byte1 = (value & 0xFF00) >> 8;
+				i2c_byte2 = value & 0xFF;
+			}
+#endif
 		} else {
 			i2c_byte1 = write_arr[i].reg_addr;
 			i2c_byte2 = (hw_dword & write_arr[i].hw_mask) >>
