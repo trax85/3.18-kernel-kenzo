6d4dfce66118d81dbc38a585f29952e1ad03e24c
Adarsh-MR <adarshmr1998@gmail.com>
2018-03-31 15:21:15 +0000

    Revert "sdcardfs: Flag files as non-mappable"
    * this has been addressed with a96f72c
    
    This reverts commit b37d8865aca4a6b74ea4701c6a5cc1962ed5f01f.

--- fs/sdcardfs/file.c
+++ fs/sdcardfs/file.c
@@ -240,7 +240,6 @@ static int sdcardfs_open(struct inode *inode, struct file *file)
 	/* save current_cred and override it */
 	OVERRIDE_CRED(sbi, saved_cred, SDCARDFS_I(inode));
 
-	file->f_mode |= FMODE_NONMAPPABLE;
 	file->private_data =
 		kzalloc(sizeof(struct sdcardfs_file_info), GFP_KERNEL);
 	if (!SDCARDFS_F(file)) {
@@ -334,11 +333,6 @@ static int sdcardfs_fasync(int fd, struct file *file, int flag)
 	return err;
 }
 
-static struct file *sdcardfs_get_lower_file(struct file *f)
-{
-	return sdcardfs_lower_file(f);
-}
-
 /*
  * Sdcardfs cannot use generic_file_llseek as ->llseek, because it would
  * only set the offset of the upper file.  So we have to implement our
@@ -361,7 +355,6 @@ out:
 	return err;
 }
 
-
 const struct file_operations sdcardfs_main_fops = {
 	.llseek		= generic_file_llseek,
 	.read		= sdcardfs_read,
@@ -376,7 +369,6 @@ const struct file_operations sdcardfs_main_fops = {
 	.release	= sdcardfs_file_release,
 	.fsync		= sdcardfs_fsync,
 	.fasync		= sdcardfs_fasync,
-	.get_lower_file = sdcardfs_get_lower_file,
 };
 
 /* trimmed directory options */
@@ -393,5 +385,4 @@ const struct file_operations sdcardfs_dir_fops = {
 	.flush		= sdcardfs_flush,
 	.fsync		= sdcardfs_fsync,
 	.fasync		= sdcardfs_fasync,
-	.get_lower_file = sdcardfs_get_lower_file,
 };
